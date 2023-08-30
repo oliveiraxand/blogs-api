30af11d740c55a7c1fa19bc514fa39b8e5383763
oliveiraxand <alexandre.osantos253@gmail.com>
2023-08-30 16:21:15 -0300

    feat: create Category Model

---
+++ src/models/Category.js
@@ -0,0 +1,20 @@
+const CategoryModel = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: {
+      type: DataTypes.INTEGER, 
+      primaryKey: true, 
+      autoIncrement: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  }, {
+    timestamps: false,
+    tableName: 'categories',
+    underscored: true
+  })
+  return Category;
+}
+
+module.exports = CategoryModel;
